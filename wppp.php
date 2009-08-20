e785f1555976878aeffd7e0c2f6dd2806670111f
Andrea Piccinelli <frasten@gmail.com>
2009-08-20 10:21:08 +0000

    Some spacing changes.

--- wppp.php
+++ wppp.php
@@ -360,7 +360,7 @@ class WPPP extends WP_Widget {
 		echo __( 'Don\'t show posts/pages with a view count under', 'wordpresscom-popular-posts' );
 		echo ": <input style='width: 50px;' id='$field_id' name='" .
 			$this->get_field_name( 'cutoff' ) . "' type='text' value='" .
-			intval( $instance['cutoff'] ) . "' /></label>" . __('(0 means unlimited)', 'wordpresscom-popular-posts' ) . '</p>';
+			intval( $instance['cutoff'] ) . "' /></label> " . __('(0 means unlimited)', 'wordpresscom-popular-posts' ) . '</p>';
 
 		$field_id = $this->get_field_id( 'list_tag' );
 		echo "<p style='text-align:right;'><label for='$field_id'>";
@@ -454,7 +454,7 @@ function WPPP_show_popular_posts( $user_args = '' ) {
 
 function wppp_notice_incompatible() {
 	echo "<div class='error'><p>" .
-	sprintf( __( "Wordpress.com Popular Post 2.0.0 is compatible with WordPress >= 2.8 only.<br />
+	sprintf( __( "Wordpress.com Popular Post &gt;= 2.0.0 is compatible with WordPress >= 2.8 only.<br />
 	Please either <a href='%s'>update</a> your WordPress installation, <a href='%s'>downgrade this plugin</a> to v1.3.5
 	or <a href='%s'>uninstall it</a>.", 'wordpresscom-popular-posts' ),
 	'http://wordpress.org/download/',
