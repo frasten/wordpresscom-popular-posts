0a4fe89052ec96d24deb0bad6b7869fb1602a28c
Andrea Piccinelli <frasten@gmail.com>
2009-12-17 21:44:29 +0000

    Fixed an issue in the uninstall process. Thanks to Joe for reporting.

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,5 @@
 <?php
-if ( !isset(ABSPATH) || !isset(WP_UNINSTALL_PLUGIN) ) return;
+if ( !defined(ABSPATH) || !defined(WP_UNINSTALL_PLUGIN) ) return;
 
 delete_option('widget_wppp');
 delete_option('wppp_cache');
